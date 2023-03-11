472d721e9db0c641207e2bc2e0dada611f596ca9
Natalya Maximova <natka-31@mail.ru>
2023-03-11 14:30:22 +0500

    редактировать верстку компоненты Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,9 @@ import classes from './Users.module.css';
 const Users = (props) => {
    if (props.users.length === 0) {
       props.setUsers([
-         { id: 1, photoUrl: 'https://i.pinimg.com/originals/a8/8b/b6/a88bb6c1cbeeafe541d381d6d15d23d1.jpg', followed: true, fullName: 'Илья', status: 'I am a boss', location: { city: 'Тюмень', country: 'Россия' } },
-         { id: 2, photoUrl: 'https://mobimg.b-cdn.net/v3/fetch/d4/d4e66fe3adbadc49d637c0e804f1e629.jpeg', followed: true, fullName: 'Наталья', status: 'I am a macovka', location: { city: 'Тюмень', country: 'Россия' } },
-         { id: 3, photoUrl: 'https://a.d-cd.net/f10fab5s-1920.jpg', followed: true, fullName: 'Зевс', status: 'I am a cat', location: { city: 'Тюмень', country: 'Россия' } },
+         { id: 1, photoUrl: 'https://webmg.ru/wp-content/uploads/2022/10/i-145-27.jpeg', followed: true, fullName: 'Илюсик', status: 'I am a boss', location: { city: 'Тюмень', country: 'Россия' } },
+         { id: 2, photoUrl: 'https://webmg.ru/wp-content/uploads/2022/05/1625736517_12-kartinkin-com-p-kotyata-anime-anime-krasivo-14.jpg', followed: true, fullName: 'Натусик', status: 'I am a macovka', location: { city: 'Тюмень', country: 'Россия' } },
+         { id: 3, photoUrl: 'https://i.pinimg.com/originals/6d/47/05/6d4705c7c8fb464de36cab7e4c0f6561.jpg', followed: true, fullName: 'Зевс', status: 'I am a cat', location: { city: 'Тюмень', country: 'Россия' } },
          { id: 4, photoUrl: 'https://www.fonstola.ru/images/201212/fonstola.ru_84796.jpg', followed: false, fullName: 'Валера', status: 'I am ...', location: { city: 'Мадрид', country: 'Испания' } },
       ])
    }
